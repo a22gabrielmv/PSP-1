4bc0c2d2e2381d974b70699632db15c13247e94a
A22GabrielMV <a22gabrielmv@iessanclemente.net>
2024-09-12 14:29:49 +0200

    m1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,43 @@
 package org.example;
+import java.sql.Array;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner reader = new Scanner(System.in); // Reading from System.in
+        System.out.println("Enter number of cases: ");
+
+        int cont=0;
+        int casos = reader.nextInt();
+
+
+        while (cont<casos){
+            int numVelas=0;
+
+            System.out.println("Enter a number: ");
+            String binario = Integer.toBinaryString(reader.nextInt());
+
+            char[] cadenaVelas=binario.toCharArray();
+
+
+            for (int i = 0; i < cadenaVelas.length; i++){
+
+                if(cadenaVelas[i] == '1'){
+                    numVelas++;
+                }
+
+            }
+
+            System.out.println(numVelas);
+
+            cont++;
+
         }
+
+
+        reader.close();
     }
 }
